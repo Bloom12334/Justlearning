5f01485b512f49c6869aaefee9f2311751e25fb5
Pavani <adhikari.ramya@mindtree.com>
2024-04-03 16:12:48 +0530

    This is the first commit

---
+++ program.py
@@ -0,0 +1,7 @@
+
+def get_vowels(string):
+    return [each for each in string if each in "aeiou"]
+get_string1 = "hello"
+get_string2 = str(input("please enter a string:"))
+print("The Vowels Are:  ",get_vowels(get_string1))
+print("The vowels are", get_vowels(get_string2))
